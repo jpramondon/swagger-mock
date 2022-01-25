07ac5b9ee88807b9227413ad16f5bb38aeab8ab2
Jerome <jpramondon@gmail.com>
2022-01-25 12:03:39 +0100

    fix: adding http method logging

--- src/middlewares/LoggingMiddleware.ts
+++ src/middlewares/LoggingMiddleware.ts
@@ -2,6 +2,6 @@ import { Request, Response, NextFunction } from "express";
 import { mainLogger } from "../Logger";
 
 export function loggingMw(req: Request, res: Response, next: NextFunction): any {
-    mainLogger.info(`${req.url} called`);
+    mainLogger.info(`${req.method} ${req.url} called`);
     next();
 }
